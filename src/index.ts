8088fadca7adc1723a3775a5b9a735b11aea8c8c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-10-07 20:46:38 +0530

    fix: start on getting file and traversal of files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { Context, Probot } from "probot";
+import { CheckDocumentation, DocumentationError } from "./linter.js";
 
 export default (app: Probot) => {
   // on pull request opened
-  app.on("pull_request.opened", async (context: Context) => {
+  app.on("pull_request.opened", async (context) => {
     await context.octokit.issues.addLabels(
       context.issue({ labels: ["awaiting review"] })
     );
+
+
+
 
     await context.octokit.issues.createComment(
       context.issue({
@@ -14,8 +18,32 @@ export default (app: Probot) => {
                [contribution guidelines](https://github.com/TheAlgorithms/C-Plus-Plus/blob/0ecb6bd28a8a6b5ffc0edcad34ac036647d2259f/CONTRIBUTING.md)",
       })
     );
-  });
 
+
+    // get read all the files
+    const files = (await (context.octokit.pulls.listFiles({
+      owner: context.repo().owner,
+      repo: context.repo().repo,
+      pull_number: context.pullRequest().pull_number,
+    }))).data;
+    
+
+
+
+    for (const file of files) {
+      let file_contents: string = ""
+      let blob = (await fetch(file.blob_url).then(r => r.blob())).text().then(value => file_contents = value);
+
+
+      let errors: DocumentationError[] = CheckDocumentation(file_contents.split("\n"));
+      for (const error of errors) {
+        // todo
+      }
+      
+
+    }
+
+  });
 
   app.on("pull_request_review.submitted", async (context) => {
     const pr = (await context.octokit.pulls.get(context.pullRequest())).data;
@@ -50,7 +78,6 @@ export default (app: Probot) => {
       await context.octokit.issues.setLabels(
         context.issue({ labels: ["approved"] })
       );
-
     }
   });
 
@@ -65,10 +92,7 @@ export default (app: Probot) => {
       })
     ).data;
 
-
-    if (
-      pr.labels.some((label) => label.id === awaiting_modification.id)
-    ) {
+    if (pr.labels.some((label) => label.id === awaiting_modification.id)) {
       await context.octokit.issues.addLabels(
         context.issue({ labels: ["awaiting review"] })
       );
