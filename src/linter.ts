b926efa291311cca0263fc3c524a5b85a7f0cf16
Stealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-11-23 11:39:41 +0530

    feat: move header comment from linter to external file

--- src/linter.ts
+++ src/linter.ts
@@ -1,3 +1,5 @@
+import { MISSING_HEADER } from './strings.js'
+
 export interface DocumentationError {
   line: number;
   what: string;
@@ -8,42 +10,30 @@ export function CheckDocumentation(file: string[]): DocumentationError[] {
   var line_count: number = 0;
 
   var previous_line: string = "";
-  var in_comment: boolean = false;
+  // var in_comment: boolean = false;
 
   for (const line of file) {
-    if (line.startsWith("/*")) {
-      in_comment = true;
-    }
+    
+    // if (line.startsWith("/*")) {
+    //   in_comment = true;
+    // }
 
     // rudimentary test for header comment
     if (line_count == 0 && !line.startsWith("/*")) {
       errors.push({
         line: 0,
-        what: `File should begin with a [comment header](https://github.com/TheAlgorithms/C-Plus-Plus/blob/master/CONTRIBUTING.md#typical-structure-of-a-program)\n\
-                \`\`\`cpp
-                /**\n
-                 * @file\n
-                 * @brief Add one-line description here. Should contain a Wikipedia\n
-                 * link or another source explaining the algorithm/implementation.\n
-                 * @details\n
-                 * This is a multi-line\n
-                 * description containing links, references,\n
-                 * math equations, etc.\n
-                 * @author [Name](https://github.com/handle)\n
-                 * @see related_file.cpp, another_file.cpp\n
-                 */\n
-                \`\`\``,
+        what: MISSING_HEADER,
       });
     }
 
     // include documenation check
-    if (line.startsWith("#include") && line.indexOf("///") == -1) {
+    if (line.startsWith("#include") && line.indexOf("//") == -1) {
       errors.push({
         line: line_count,
         what: `All includes must be documented with what functions of the header this file uses.\n
-               \`\`\`cpp\n
-               #include <cassert>   /// for assert\n
-               #include <cstdint>   /// for std::uint32_t\n
+               \`\`\`cpp
+               #include <cassert>   /// for assert
+               #include <cstdint>   /// for std::uint32_t
                \`\`\`\n
                `,
       });
@@ -53,20 +43,21 @@ export function CheckDocumentation(file: string[]): DocumentationError[] {
     if (line.startsWith("namespace") && !previous_line.endsWith("*/")) {
       errors.push({
         line: line_count,
-        what: `Namespaces must be documented\n
-                \`\`\`cpp\n
-                /**\n
-                 * @namespace\n
-                 * @brief <namespace description>\n
-                 */\n
-                 namespace name {\n
+        what: `Namespaces must be documented
+                \`\`\`cpp
+                /**
+                 * @namespace
+                 * @brief <namespace description>
+                 */
+                 namespace name {
                 \`\`\``,
       });
     }
 
-    if (line.endsWith("*/")) {
-      in_comment = false;
-    }
+    // if (line.endsWith("*/")) {
+    //   in_comment = false;
+    // }
+    previous_line = line;
     line_count++;
   }
   return errors;
---
+++ src/strings.ts
@@ -0,0 +1,16 @@
+export const MISSING_HEADER = `File should begin with a [comment header](https://github.com/TheAlgorithms/C-Plus-Plus/blob/master/CONTRIBUTING.md#typical-structure-of-a-program)
+\`\`\`cpp
+/**
+ * @file
+ * @brief Add one-line description here. Should contain a Wikipedia
+ * link or another source explaining the algorithm/implementation.
+ * @details
+ * This is a multi-line
+ * description containing links, references,
+ * math equations, etc.
+ * @author [Name](https://github.com/handle)
+ * @see related_file.cpp, another_file.cpp
+ */
+\`\`\``
+
+export const MISSING_FUNCTION_DOCS = `Functions must be documented`
